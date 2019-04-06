49f11b534b652dda26692e27ec4e5ceb78285b14
126_100006497377_DataStreams <126_100006497377_DataStreams@git.cloud.tencent.com>
2019-04-06 12:44:24 +0800

    final version

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const router = require('koa-router')()
+
+router.get('/', async (ctx, next) => {
+  ctx.body = {
+    username: "叶田地",
+    sex: "男"
+  }
+  //await ctx.render('index')
+})
+
+router.get('/string', async (ctx, next) => {
+  ctx.body = 'koa2 string'
+})
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+})
+
+module.exports = router
